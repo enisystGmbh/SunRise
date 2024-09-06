b16c3d3ed441f962a1becb9dcce09249203348ed
rohime <37575264+rohime@users.noreply.github.com>
2024-09-06 10:10:51 +0200

    Define methods in class

--- make-header-only.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-HEADER_ONLY_DIR="header-only"
-
-mkdir -p "$HEADER_ONLY_DIR"
-printf '%s\n\n// INCLUDE SunRise.cpp BEGIN\n\n%s\n\n// INCLUDE SunRise.cpp END\n\n#endif' "$(head -n -1 SunRise.h)" "$(sed '/#include "SunRise.h"/d' SunRise.cpp)" > "$HEADER_ONLY_DIR/SunRise.h"
